fa1fecfb684c2e6e1412395a5684dffa1ed984e0
PeterClaydon <peter.claydon@continuumbridge.com>
2015-02-17 10:01:08 +0000

    WIP

---
+++ adaptor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Copyright (C) ContinuumBridge Limited, 2014 - All Rights Reserved
+# Written by Peter Claydon
+#
+import sys
+from adaptor_a import Adaptor
+Adaptor(sys.argv)
